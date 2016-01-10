1dd863357ac5c68b0bbc94ff33646cc09346d375
Jeremy <jeremy@jeremylee.co.nz>
2016-01-10 16:42:26 +1300

    Setup cards and asks user for wager amount

--- blackjack.py
+++ blackjack.py
@@ -1,3 +1,6 @@
+NUMBER_OF_DECKS = 1
+
+
 def setup_game():
     print("Welcome to Blackjack!")
     money = 0
@@ -11,6 +14,17 @@ def setup_game():
             print("Please enter valid amount.")
 
     return money
+
+
+def reset_game():
+    single_suit = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+    deck_of_cards = []
+
+    for i in range(NUMBER_OF_DECKS):
+        deck_of_cards.extend(single_suit)
+        deck_of_cards.extend(single_suit)
+        deck_of_cards.extend(single_suit)
+        deck_of_cards.extend(single_suit)
 
 
 def is_valid_dollar_amount(user_input):
@@ -27,3 +41,17 @@ def is_valid_dollar_amount(user_input):
 
 
 total_money = setup_game()
+
+while True:
+    reset_game()
+
+    while True:
+        wager = input("How much would you like to wager? ")
+
+        valid_wager, wager = is_valid_dollar_amount(wager)
+        if valid_wager and wager <= total_money:
+            break
+        else:
+            print("Please enter valid amount.")
+
+    # begin game
